d18be7abd9edd75777a8f62432fb7a6e803d7ed7
99003735 <78857812+99003735@users.noreply.github.com>
2021-02-28 22:30:57 +0530

    Update time.c

--- assignment-1/threads/time.c
+++ assignment-1/threads/time.c
@@ -1,1 +1,18 @@
+#include <stdio.h> 
+#include <time.h> 
+int main() 
+{ 
+	time_t s, val = 1; 
+	struct tm* time_is; 
+	s = time(NULL); 
+	time_is = localtime(&s); 
+
+	
+	printf("%02d:%02d:%02d", 
+		time_is->tm_hour, 
+		time_is->tm_min, 
+		time_is->tm_sec); 
+
+	return 0; 
+} 
 
