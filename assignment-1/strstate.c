fd23b13811101a83401f12cef9bd8ee53765622e
99003735 <78857812+99003735@users.noreply.github.com>
2021-02-28 22:16:56 +0530

    Add files via upload

---
+++ assignment-1/strstate.c
@@ -0,0 +1,66 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE * file_1;
+    char sentence[50];
+
+    char c;
+    int characters, words, lines;
+
+
+    //enter the path (f.text)
+    printf("Enter the path: ");
+    scanf("%s",sentence);
+
+    //opening in read mode
+    file_1 = fopen(sentence, "read");
+
+
+   //check if it open successfully if any error output as printf statement as printf command
+    if (file_1 == NULL)
+    {
+        printf("\nUnable to open file.\n");
+        printf("Please check if file exists and you have read privilege.\n");
+
+        exit(EXIT_FAILURE);
+    }
+
+    
+      //Logic to count characters, words and lines.
+
+    characters = words = lines = 0;
+    while ((c = fgetc(file_1)) != EOF)
+    {
+        characters++;
+
+        // Check new line 
+        if (c == '\n' || c == '\0')
+            lines++;
+
+        // Check words 
+        if (c == ' ' || c == '\t' || c == '\n' || c == '\0')
+            words++;
+    }
+
+    /* Increment words and lines for last word */
+    if (characters > 0)
+    {
+        words++;
+        lines++;
+    }
+
+    //printing the result
+    printf("\n");
+    printf("Total characters = %d\n", characters);
+    printf("Total words      = %d\n", words);
+    printf("Total lines      = %d\n", lines);
+
+
+    /* Close files to release resources */
+    fclose(file_1);
+
+    return 0;
+}
---
+++ assignment-1/targetprocess.c
@@ -0,0 +1,20 @@
+#include<unistd.h>
+#include<signal.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+void target(int num)
+{
+    write(4,"Kill process\n",9);
+
+}
+
+int main()
+{
+    signal(SIGKILL,target);
+    while(1)
+    {
+        printf("process ID is = %d\n",getpid());
+        sleep(1);
+    }
+}
