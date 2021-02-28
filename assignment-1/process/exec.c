464effb2cdd8ac29037a2de57d1fde096e086ec2
99003735 <78857812+99003735@users.noreply.github.com>
2021-02-28 22:33:45 +0530

    Update exec.c

--- assignment-1/process/exec.c
+++ assignment-1/process/exec.c
@@ -1,1 +1,34 @@
 
+#include<unistd.h>
+#include<stdio.h>
+int main()
+{
+ pid_t ret;
+ char p[100],q[100];
+ printf("enter for compilation\n");
+ gets(p);
+  printf("enter for compilation\n");
+ gets(q);
+ ret=fork();
+  if(ret==0)
+    {
+
+ execlp(p,NULL);
+
+    }
+    else
+    {
+    int ret1=fork();
+    if(ret1==0)
+    {
+    execlp(q,NULL);
+    }
+    else
+    {
+    }
+    }
+    printf("Thank you\n");
+    }
+    
+    
+    
